902f0140f1bc0952153ad690e1a76f738073b4a6
ArifRamadhan <arifardhan24@gmail.com>
2019-11-05 21:32:23 +0700

    Add view sebelum user memasukkan jumlah yang mau ditebus

--- src/co/id/pegadaian/sprint/b5/Tebus.java
+++ src/co/id/pegadaian/sprint/b5/Tebus.java
@@ -21,6 +21,15 @@ public class Tebus {
         long price = 0;
         int utang=0;
         int tebus = 0;
+
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("|ID|                PRODUCT|          PRICE|           STATUS|             UTANG|");
+        System.out.println("---------------------------------------------------------------------------------");
+        for (int i=0; i< dataArrayList.size(); i++) {
+            System.out.println("| " +dataArrayList.get(i).getId()+"|             " +dataArrayList.get(i).getProductCategory() +"|      "+dataArrayList.get(i).getPrice() +"|         "+dataArrayList.get(i).getStatus()  +"|      "+dataArrayList.get(i).getPrice());    
+            
+            }
+
                 
         
 	}
