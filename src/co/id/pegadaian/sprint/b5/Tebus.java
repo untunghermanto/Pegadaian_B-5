a1ad0703c2df934c69d7764d321586d6b6b2e747
ArifRamadhan <arifardhan24@gmail.com>
2019-11-05 21:27:55 +0700

    Create Tebus class + variable

---
+++ src/co/id/pegadaian/sprint/b5/Tebus.java
@@ -0,0 +1,27 @@
+package co.id.pegadaian.sprint.b5;
+import co.id.pegadaian.sprint.b5.data.Data;
+import co.id.pegadaian.sprint.b5.data.Read;
+import co.id.pegadaian.sprint.b5.data.Write;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+import java.util.Scanner;
+
+public class Tebus {
+	
+	public void tebus() {
+		
+		Read read = new Read();
+        ArrayList<Data> dataArrayList = read.readData();
+        
+        Scanner inputMenu = new Scanner(System.in);
+        boolean loopStatus = true;
+        long price = 0;
+        int utang=0;
+        int tebus = 0;
+                
+        
+	}
+}
