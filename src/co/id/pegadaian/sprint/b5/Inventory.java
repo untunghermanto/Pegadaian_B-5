fc9cfcc6942991c8369b8df40f396ebc2c2ecc3c
untunghermanto <57380947+untunghermanto@users.noreply.github.com>
2019-11-05 15:49:17 +0700

    merging inventory and master

--- src/co/id/pegadaian/sprint/b5/Inventory.java
+++ src/co/id/pegadaian/sprint/b5/Inventory.java
@@ -1,34 +1,35 @@
 package co.id.pegadaian.sprint.b5;
 
 public class Inventory {
-	private int id;
-	private String nama;
-	private String produk;
-	private String deskripsi;
-	private int harga;
-	public Inventory(int id, String nama, String produk, String deskripsi, int harga) {
-		super();
-		this.id = id;
-		this.nama = nama;
-		this.produk = produk;
-		this.deskripsi = deskripsi;
-		this.harga = harga;
-	}
-	public int getId() {
-		return id;
-	}
-	public String getNama() {
-		return nama;
-	}
-	public String getProduk() {
-		return produk;
-	}
-	public String getDeskripsi() {
-		return deskripsi;
-	}
-	public int getHarga() {
-		return harga;
-	}
+//	private int id;
+//	private String nama;
+//	private String produk;
+//	private String deskripsi;
+//	private int harga;
+//	public Inventory(int id, String nama, String produk, String deskripsi, int harga) {
+//		super();
+//		this.id = id;
+//		this.nama = nama;
+//		this.produk = produk;
+//		this.deskripsi = deskripsi;
+//		this.harga = harga;
+//	}
+//	public int getId() {
+//		return id;
+//	}
+//	public String getNama() {
+//		return nama;
+//	}
+//	public String getProduk() {
+//		return produk;
+//	}
+//	public String getDeskripsi() {
+//		return deskripsi;
+//	}
+//	public int getHarga() {
+//		return harga;
+//	}
 	
+
 	
 }
