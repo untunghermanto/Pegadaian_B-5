5d454664f6822f6ac06ba3d327e328526d967a88
untunghermanto <57380947+untunghermanto@users.noreply.github.com>
2019-11-05 21:44:43 +0700

    commit output table formatting

--- src/co/id/pegadaian/sprint/b5/Main.java
+++ src/co/id/pegadaian/sprint/b5/Main.java
@@ -4,54 +4,49 @@ import co.id.pegadaian.sprint.b5.data.Data;
 import co.id.pegadaian.sprint.b5.data.Read;
 import co.id.pegadaian.sprint.b5.data.Write;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+    	boolean notExitStatus = true;
 
-		Data dataGadai = new Data();
+    	do {
+			Scanner inputMenu=new Scanner(System.in);
+			System.out.println("Pegadaian System");
 
-    	Scanner inputMenu=new Scanner(System.in);
-		System.out.println("Pegadaian System");
-		for (int i = 0; i < 16; i++) {
-			System.out.print('=');
+			for (int i = 0; i < 16; i++) {
+				System.out.print('=');
+			}
+
+			System.out.println();
+			System.out.println("1. Gadai\n2. Tebus\n3. Tampilkan Inventory\n4. Exit");
+
+			try {
+				System.out.print("Opsi :");
+
+				int pilihanMenu = inputMenu.nextInt();
 			
-		}
-		System.out.println();
-		System.out.println("1. Gadai\n2. Tebus\n3. Tampilkan Inventory\n4. Exit");
-		boolean notExitStatus=true;
-		do {
-			
-			try {System.out.print("Opsi :");
-			int pilihanMenu=inputMenu.nextInt();
-			
-				if (pilihanMenu>4 ||pilihanMenu<1) {
+				if (pilihanMenu > 4 || pilihanMenu < 1) {
 					System.out.println("Inputan yang anda masukkan salah");
-				}
-				else if (pilihanMenu==1) {
+				} else if (pilihanMenu == 1) {
 					new Gadai().gadai();
-				}
-				else if(pilihanMenu == 2) {
+				} else if(pilihanMenu == 2) {
 			
-				}
-				else if(pilihanMenu==3){
-				}
-				else {
+				} else if(pilihanMenu == 3){
+					new Inventory().showDataInventory();
+				} else {
 					System.out.println("Aplikasi Ditutup");
 					System.out.println("Bye");
-					notExitStatus=false;
+					notExitStatus = false;
 				}
-				
-			}catch(Exception e){
+			} catch(Exception e){
 				System.out.println("Inputan yang anda masukkan salah");
 				inputMenu.next();
 				continue;
-		
 			}
-		}while (notExitStatus);
-
-
+		} while (notExitStatus);
     }
 }
