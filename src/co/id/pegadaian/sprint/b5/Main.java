a06324de5a3688bb0f75940d5a15c4feada1641f
nrfzh6690 <57356667+nrfzh6690@users.noreply.github.com>
2019-11-05 14:36:12 +0700

    update main menu

--- src/co/id/pegadaian/sprint/b5/Main.java
+++ src/co/id/pegadaian/sprint/b5/Main.java
@@ -1,7 +1,46 @@
 package co.id.pegadaian.sprint.b5;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+    	Scanner inputMenu=new Scanner(System.in);
+		System.out.println("Pegadaian System");
+		for (int i = 0; i < 16; i++) {
+			System.out.print('=');
+			
+		}
+		System.out.println();
+		System.out.println("1. Gadai\n2. Tebus\n3. Tampilkan Inventory\n4. Exit");
+		boolean notExitStatus=true;
+		do {
+			
+			try {System.out.print("Opsi :");
+			int pilihanMenu=inputMenu.nextInt();
+			
+				if (pilihanMenu>4 ||pilihanMenu<1) {
+					System.out.println("Inputan yang anda masukkan salah");
+				}
+				else if (pilihanMenu==1) {
+				}
+				else if(pilihanMenu == 2) {
+			
+				}
+				else if(pilihanMenu==3){
+				}
+				else {
+					System.out.println("Aplikasi Ditutup");
+					System.out.println("Bye");
+					notExitStatus=false;
+				}
+				
+			}catch(Exception e){
+				System.out.println("Inputan yang anda masukkan salah");
+				inputMenu.next();
+				continue;
+		
+			}
+		}while (notExitStatus);
 
     }
 }
