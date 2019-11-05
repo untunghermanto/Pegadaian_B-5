192f5f85837b16a0b1317a47fd3d00987c56cc7b
samatra marwa hany <samatramh5@gmail.com>
2019-11-05 22:13:28 +0700

    Add Logic (Bayar utang) Tebus

--- src/co/id/pegadaian/sprint/b5/Tebus.java
+++ src/co/id/pegadaian/sprint/b5/Tebus.java
@@ -19,14 +19,14 @@ public class Tebus {
         Scanner inputMenu = new Scanner(System.in);
         boolean loopStatus = true;
         long price = 0;
-        int utang=0;
+        long utang=0;
         int tebus = 0;
         int id = 0;
         System.out.println("---------------------------------------------------------------------------------");
         System.out.println("|ID|                PRODUCT|          PRICE|           STATUS|             UTANG|");
         System.out.println("---------------------------------------------------------------------------------");
         for (int i=0; i< dataArrayList.size(); i++) {
-            System.out.println("| " +dataArrayList.get(i).getId()+"|             " +dataArrayList.get(i).getProductCategory() +"|      "+dataArrayList.get(i).getPrice() +"|         "+dataArrayList.get(i).getStatus()  +"|      "+dataArrayList.get(i).getPrice());    
+            System.out.println("| " +dataArrayList.get(i).getId()+"|             " +dataArrayList.get(i).getProductCategory() +"|      "+dataArrayList.get(i).getPrice() +"|         "+dataArrayList.get(i).getStatus()  +"|      "+dataArrayList.get(i).getUtang());    
             
             }
         System.out.println("---------------------------------------------------------------------------------");
@@ -55,6 +55,39 @@ public class Tebus {
                 continue;
             }
         }
+        loopStatus = true;
+        while(loopStatus) {
+            try {
+                System.out.print("Masukkan uang yang ingin anda bayarkan:");
+                utang=inputMenu.nextLong();
+                int flag=0;
+                for (Data data : dataArrayList) {
+                	if (data.getId() == id) {
+                		long sisa = 0;
+                		sisa = data.getUtang()-utang; 
+                		if(sisa>0)
+                		{
+                			data.setUtang(sisa);
+                			System.out.println("pembayaran telah diterima. Sisa hutang anda adalah "+sisa);
+                		}
+                		loopStatus = false;
+                		flag = 1;
+                		break;
+                    } 
+                }
+                if (flag==0) {
+                	System.out.println("Id tidak ada");
+                    
+				}
+                
+
+            }catch(Exception e){
+                System.out.println("Inputan yang anda masukkan salah");
+                inputMenu.next();
+                continue;
+            }
+        }
+        
 
                 
         
