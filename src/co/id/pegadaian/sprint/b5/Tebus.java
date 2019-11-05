1dcab3edd0cb5567e698ef807849cab01672eea2
untunghermanto <57380947+untunghermanto@users.noreply.github.com>
2019-11-05 22:57:19 +0700

    tebus finalization

--- src/co/id/pegadaian/sprint/b5/Tebus.java
+++ src/co/id/pegadaian/sprint/b5/Tebus.java
@@ -10,33 +10,30 @@ import java.util.Scanner;
 import java.util.Scanner;
 
 public class Tebus {
-
 	public static void tebus() {
-
 		Read read = new Read();
+
 		ArrayList<Data> dataArrayList = read.readData();
 
 		Scanner inputMenu = new Scanner(System.in);
+
 		boolean loopStatus = true;
+
 		long price = 0;
 		long utang=0;
+
 		int tebus = 0;
 		int id = 0;
-		System.out.println("---------------------------------------------------------------------------------");
-		System.out.println("|ID|                PRODUCT|          PRICE|           STATUS|             UTANG|");
-		System.out.println("---------------------------------------------------------------------------------");
-		for (int i=0; i< dataArrayList.size(); i++) {
-			if(dataArrayList.get(i).getStatus().equalsIgnoreCase("Lunas"))
-			{
-				
-			}else
-			{
-				System.out.println("| " +dataArrayList.get(i).getId()+"|             " +dataArrayList.get(i).getProductCategory() +"|      "+dataArrayList.get(i).getPrice() +"|         "+dataArrayList.get(i).getStatus()  +"|      "+dataArrayList.get(i).getUtang());    
-	
+        String formatter = "| %-4s | %-15s | %-10s | %-25s | %-15s |";
+        System.out.println("+------+-----------------+------------+---------------------------+-----------------+");
+        System.out.println(String.format(formatter, "ID", "Product", "Price", "Status", "Utang"));
+        System.out.println("+------+-----------------+------------+---------------------------+-----------------+");
+		for (Data value : dataArrayList) {
+			if (!value.getStatus().equalsIgnoreCase("Lunas")) {
+			    System.out.println(String.format(formatter, value.getId(), value.getProductCategory(), value.getPrice(), value.getStatus(), value.getUtang()));
 			}
-			
 		}
-		System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("+------+-----------------+------------+---------------------------+-----------------+");
 
 		while(loopStatus) {
 			try {
@@ -52,10 +49,7 @@ public class Tebus {
 				}
 				if (flag==0) {
 					System.out.println("Id tidak ada");
-
 				}
-
-
 			}catch(Exception e){
 				System.out.println("Inputan yang anda masukkan salah");
 				inputMenu.next();
@@ -68,20 +62,17 @@ public class Tebus {
 				System.out.print("Masukkan uang yang ingin anda bayarkan:");
 				utang=inputMenu.nextLong();
 				int flag=0;
-				for(int i =0;i<dataArrayList.size();i++)
-				{
+				for(int i =0;i<dataArrayList.size();i++) {
 					if (dataArrayList.get(i).getId() == id) {
 						long sisa = 0;
-						sisa = dataArrayList.get(i).getUtang()-utang; 
-						if(sisa>0)
-						{
+						sisa = dataArrayList.get(i).getUtang()-utang;
+						if (sisa>0) {
 							dataArrayList.get(i).setUtang(sisa);
 							System.out.println("Pembayaran telah diterima. Sisa hutang anda adalah "+sisa);
 							loopStatus = false;
 							flag = 1;
 							break;
-						}else if(sisa == 0)
-						{
+						} else if(sisa == 0) {
 							dataArrayList.get(i).setUtang(sisa);
 							dataArrayList.get(i).setStatus("Lunas");
 							System.out.println("Pinjaman anda telah lunas.");
@@ -89,20 +80,15 @@ public class Tebus {
 							loopStatus = false;
 							flag = 1;
 							break;
-						}else if(sisa<0)
-						{
+						} else if(sisa<0) {
 							System.out.println("Pembayaran anda melebihi sisa pinjaman anda. Sisa pinjaman anda adalah "+dataArrayList.get(i).getUtang());
 							flag = 1;
 						}
-
-					} 
+					}
 				}
 				if (flag==0) {
 					System.out.println("Id tidak ada");
-
 				}
-
-
 			}catch(Exception e){
 				System.out.println("Inputan yang anda masukkan salah");
 				inputMenu.next();
