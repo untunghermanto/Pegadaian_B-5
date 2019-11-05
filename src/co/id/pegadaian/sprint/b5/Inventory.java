6486d677094e5a6af78f47e99940654ff2c560f6
natashaasrh <57379954+natashaasrh@users.noreply.github.com>
2019-11-05 20:26:20 +0700

    Show data inventory

--- src/co/id/pegadaian/sprint/b5/Inventory.java
+++ src/co/id/pegadaian/sprint/b5/Inventory.java
@@ -6,15 +6,19 @@ import co.id.pegadaian.sprint.b5.data.Read;
 
 public class Inventory {
 
-	public Inventory() {
+	public void showDataInventory() {
+		Read read = new Read();
 		
-	int i;
-	Read read = new Read();
-    ArrayList<Data> dataArrayList = read.readData();
+	    ArrayList<Data> dataArrayList = read.readData();
+		
+		System.out.println("---------------------------------------------------------------------------------");
+		System.out.println("|ID|                NAMA|          PRODUCT|           DESCRIPTION|         PRICE|");
+		System.out.println("---------------------------------------------------------------------------------");
 	
-	System.out.println("---------------------------------------------------------------------------------");
-	System.out.println("|ID|                NAMA|          PRODUCT|           DESCRIPTION|         PRICE|");
-	System.out.println("---------------------------------------------------------------------------------");
+		for (int i=0; i< dataArrayList.size(); i++) {
+			System.out.println("| " +dataArrayList.get(i).getId() + "|    " +dataArrayList.get(i).getNama() + "|       " +dataArrayList.get(i).getProductCategory() + "|         "+dataArrayList.get(i).getDescription()  +"|      "+dataArrayList.get(i).getPrice());	
+			
+			}
 	}
 
 
